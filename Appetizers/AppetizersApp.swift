804e2874d23ae5765249d3bb11a95e2634f92034
Anastasia <anastasia_martiniuc@homedepot.com>
2024-01-16 11:32:00 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by amartiniuc on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
